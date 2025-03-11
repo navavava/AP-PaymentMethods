db5222cf29f35b1b5fbb9fb78f83e273415a4f10
Nava <navajay2018@gmail.com>
2025-03-11 17:34:56 +0330

    customer class

--- src/payment/Customer.java
+++ src/payment/Customer.java
@@ -1,4 +1,26 @@
 package payment;
 
-public class Customer {
+import java.util.ArrayList;
+
+public abstract class Customer {
+
+    String name;
+    ArrayList<String> paymentHistory;
+
+    public Customer(String name){
+        this.name = name;
+        paymentHistory = new ArrayList<>();
+    }
+
+    public abstract void displayCustomerInfo();
+    public void makePayment(PaymentStrategy paymentStrategy, double amount){
+        paymentStrategy.pay(amount);
+        paymentHistory.add("amount: " + amount + paymentStrategy.getPaymentDetails());
+    }
+
+    public void showPaymentHistory(){
+        for(String paymentHistory : paymentHistory){
+            System.out.println(paymentHistory);
+        }
+    }
 }
